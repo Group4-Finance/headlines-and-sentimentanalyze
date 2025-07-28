34c8cc32bbb94279ba451b6a6d36a1076b928503
JeremyJHAO <andrejhao0212@gmail.com>
2025-07-28 19:13:34 +0800

    Update megabank_news.py

--- today/megabank_news.py
+++ today/megabank_news.py
@@ -9,15 +9,17 @@ jieba.setLogLevel(logging.WARNING)
 
 table = []
 headers = {
-    "User-Agent": "Mozilla/5.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
 }
 
+# 只抓今天
 today = datetime.today().date()
 today_str = today.strftime("%Y%m%d")
+date_str = f"{today.year}-{today.month}-{today.day}"
 formatted_date = today.strftime("%Y/%m/%d")
 
-for page in range(1, 6):
-    url = f"https://fund.megabank.com.tw/w/wp/wu01megaNews.djhtm?A=NA&B={today.year}-{today.month}-{today.day}&C=NA&Page={page}"
+for page in range(1, 6):  # 每日最多5頁
+    url = f"https://fund.megabank.com.tw/w/wp/wu01megaNews.djhtm?A=NA&B={date_str}&C=NA&Page={page}"
     response = requests.get(url, headers=headers)
     html = BeautifulSoup(response.text, features="html.parser")
 
@@ -28,10 +30,18 @@ for page in range(1, 6):
         if en_date and en_title and en_date.text.strip() == formatted_date:
             title_final = en_title.text.strip()
             url_final = "https://fund.megabank.com.tw" + en_title['href']
-            table.append([formatted_date, title_final, url_final])
+            tags = jieba.analyse.extract_tags(title_final)
 
-df = pd.DataFrame(table, columns=["時間", "標題", "連結"])
-filename = f"megabank_news_{today_str}.csv"
-df.to_csv(filename, index=False, encoding="utf-8-sig")
+            data = {
+                "日期": en_date.text.strip(),
+                "標題": title_final,
+                "連結": url_final,
+                "標籤": tags
+            }
 
-print(f"✅ 共儲存 {len(table)} 筆今日兆豐金新聞 → {filename}")
+            if data not in table:
+                table.append(data)
+
+# 存成 CSV
+df = pd.json_normalize(table)
+df.to_csv(f"megabank_news_{today_str}.csv", index=False, encoding="utf-8-sig")
